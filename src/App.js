e703d915c52b7fc7e8af20e6303a3de3127605ad
Jang.hyunjeong <jang.hyunjeong01@gmail.com>
2022-03-11 22:51:17 +0900

    ⚡️ useEffect  , 함수의 실행시점 정하기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import { useEffect, useState } from "react"; //01. useState , useEffect import
-//import React from "react";  //React자체를 가져오면, React.useState  React.useEffect 등의 형태로 쓰면 잘 된다.
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onChange = (event) => setKeyword(event.target.value);
   const onClick = () => setValue((prev) => prev + 1);
   
-  console.log("i run all the time"); // 클릭시 계속 실행되어야 함수
+  console.log("i run all the time"); 
 
-  useEffect( () => { //02.useEffect 함수 안에 맨처음에만 실행하고자 하는 함수를 넣는다.
-    console.log("CALL THE API..."); // 맨처음에만 실행되고, 다시는 실행되지 않을 함수
+  //01. 새로고침시, 한번만 실행
+  useEffect( () => {
+    console.log("I run only once"); 
   }, []);
+
+  //02. keyword가 변화할때만 코드를 실행하고싶다.
+  useEffect( () => {
+      console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+
+  //03. counter가 변화할때만 코드를 실행하고싶다.
+  useEffect( () => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+
+   //04. keyword 와 counter가 변화할때만 코드를 실행하고싶다.
+   useEffect( () => {
+    console.log("I run when 'keyword' and 'counter' changes.");
+  }, [keyword, counter]);
+
   
   return (
     <div>
+      <input 
+      value={keyword}
+      onChange={onChange} 
+      type="text" 
+      placeholder="Search here..."
+      >
+      </input>
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
   );
 }
-//component가 맨처음 실행될때만 실헹되고, 디시는 실행되지 않도록 하고싶을대, 
 
 export default App;
