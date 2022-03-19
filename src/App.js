3f441635825347e6d5e3a109fb6299214e41b97c
Jang.hyunjeong <jang.hyunjeong01@gmail.com>
2022-03-20 01:06:25 +0900

    투두리스트 만들기

--- src/App.js
+++ src/App.js
@@ -1,45 +1,47 @@
-import { useEffect, useState } from "react"; //01. useState , useEffect import
+import { useState } from "react";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onChange = (event) => setKeyword(event.target.value);
-  const onClick = () => setValue((prev) => prev + 1);
-  
-  console.log("i run all the time"); 
-
-  //01. 새로고침시, 한번만 실행
-  useEffect( () => {
-    console.log("I run only once"); 
-  }, []);
-
-  //02. keyword가 변화할때만 코드를 실행하고싶다.
-  useEffect( () => {
-      console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-
-  //03. counter가 변화할때만 코드를 실행하고싶다.
-  useEffect( () => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
-
-   //04. keyword 와 counter가 변화할때만 코드를 실행하고싶다.
-   useEffect( () => {
-    console.log("I run when 'keyword' and 'counter' changes.");
-  }, [keyword, counter]);
-
-  
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === ""){
+      return;
+    } else {
+      setToDo("");
+      setToDos(currentArray => [toDo, ...currentArray]);
+      // setToDos("oldToDOs" => ["new ToDo", [oldToDOs]]);
+      // 배열안에 다른 배열을 요소형태로 추가하고싶다면 ...배열명 형태와 같은 모양으로 삽입해야한다.
+    };
+  };
+  console.log(toDos);
   return (
     <div>
-      <input 
-      value={keyword}
-      onChange={onChange} 
-      type="text" 
-      placeholder="Search here..."
-      >
-      </input>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form>
+        <input 
+        onChange={onChange}
+        value={toDo}
+        type="text" 
+        placeholder="Write your to dos"
+        >
+        </input>
+        <button 
+        onClick={onSubmit}
+        type="submit"
+        >Submit</button>
+        </form>
+        <hr/>
+        <ul>
+        {toDos.map((item,index) => <li key={index}>{item}</li> ) }
+        {/* 
+        ** map? 예전 array를 가져와서 변형해줌.
+        ** 같은 component의 list를 render 할때 key라는 props를 넣어주어야한다. 
+        이건 그냥 react가 기본적으로 list에 있는 모든 item들을 인식하기 때문.
+        이럴땐 그냥 ket만 넣으면 됌
+        */}
+        </ul>
     </div>
   );
 }
