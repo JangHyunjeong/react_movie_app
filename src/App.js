2aa4648862b47c428227f2cc7bfa8eb55e03769c
Jang.hyunjeong <jang.hyunjeong01@gmail.com>
2022-03-26 01:12:43 +0900

    Movie App part One

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [loading, setloading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  useEffect(() => {
-    fetch('https://api.coinpaprika.com/v1/tickers').then((response) => response.json())
-    .then((json) => {
-      setCoins(json);
-      setloading(false);
-    });
-  },[])
-  // this code works only once
+  const [loading, setLoading] = useState(true);
+  // data , data를 수정할 수 있는 함수
+  const [movies, setMovies] = useState([]);
+  // useEffect(() => {
+  //   fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year').then((response) => response.json())
+  //   .then((json) => {
+  //     setMovies(json.data.movies);
+  //     setLoading(false);
+  //   });
+  // },[])
+  const getmovies = async() => {
+    // const response = await fetch(
+    //   'https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year'
+    // );
+    // const json = await response.json();
 
+    const json = await ( 
+      await fetch(
+      'https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year'
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  }
+  useEffect(() => {
+    getmovies();
+  }, [])
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? ( <strong>Loading...</strong>
-      ) : (
-      <select>
-        {coins.map((coin) => <option>{coin.name} ({coin.symbol}) : {coin.quotes.USD.price} </option>)}
-      </select>
-      )}  
+     {loading ? (
+      <h1>loading</h1>
+     ) : (
+       <div>
+       {movies.map((movie) => (
+         <div key={movie.id}>
+          <img src={movie.medium_cover_image}></img>
+          <h2>{movie.title}</h2>
+          <p>{movie.summary}</p>
+          <ul>
+            {movie.genres.map((g)=> (
+              <li key={g}>{g}</li>
+            ))}
+          </ul>
+         </div>
+       ))}
+     </div>  
+     )}
     </div>
   );
 }
